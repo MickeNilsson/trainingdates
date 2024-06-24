b16dd82b5cccdb8a478f9f834ef78f1ae83a4b74
Mikael Nilsson <mail@mikael-nilsson.se>
2024-06-24 19:18:14 +0100

    Display member data

--- src/components/OtherMemberHome.jsx
+++ src/components/OtherMemberHome.jsx
@@ -11,7 +11,7 @@ function OtherMemberHome(props) {
     useEffect(() => {
 
         const params_o = {
-            fields: 'maindescription',
+            fields: 'maindescription,country,city,weight,height,haircolor,eyecolor,physique,alcohol,smoke,marital,children,wantchildren,profession,partnerpreferences,genderseek,agefromseek,agetoseek,countryseek',
             id: member.id
         };
 
@@ -45,7 +45,48 @@ function OtherMemberHome(props) {
 
                         <Card.Body>
 
-                            {member.maindescription}
+                            <p style={{color: 'rgb(70, 193, 187)'}}>I'm...</p>
+
+                            <p>{member.maindescription}</p>
+
+                            <p>I live in {member.city}, {member.country}</p>
+
+                            <p style={{color: 'rgb(70, 193, 187)'}}>I'm looking for...</p>
+
+                            A {member.genderseek} between {member.agefromseek} to {member.agetoseek} living in {member.countryseek}.&nbsp;
+
+                            {member.partnerpreferences}
+
+                        </Card.Body>
+
+                    </Card>
+
+                </Col>
+
+                <Col className='mb-3'>
+
+                    <Card style={{ height: '376px' }}>
+
+                        <Card.Header className='card-header'>
+
+                            Facts about me
+
+                        </Card.Header>
+
+                        <Card.Body>
+
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Profession: </span><span style={{textTransform: 'capitalize'}}>{member.profession}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Physique: </span><span style={{textTransform: 'capitalize'}}>{member.physique}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Marital status: </span><span style={{textTransform: 'capitalize'}}>{member.marital}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Have children: </span><span style={{textTransform: 'capitalize'}}>{member.children}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Want (more) children: </span><span style={{textTransform: 'capitalize'}}>{member.wantchildren}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Hair color: </span><span style={{textTransform: 'capitalize'}}>{member.haircolor}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Eye color: </span><span style={{textTransform: 'capitalize'}}>{member.eyecolor}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Height: </span><span style={{textTransform: 'capitalize'}}>{member.height}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Weight: </span><span style={{textTransform: 'capitalize'}}>{member.weight}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Alcohol: </span><span style={{textTransform: 'capitalize'}}>{member.alcohol}</span><br />
+                            <span  style={{color: 'rgb(70, 193, 187)'}}>Smoke: </span><span style={{textTransform: 'capitalize'}}>{member.smoke}</span><br />
+
 
                         </Card.Body>
 
@@ -54,6 +95,8 @@ function OtherMemberHome(props) {
                 </Col>
 
             </Row>
+
+            
 
         </Container>
     );
